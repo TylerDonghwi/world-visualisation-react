5c66b82b5c3fa50d02b1f28631bd14bb092a8b42
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-08-08 22:19:01 +1200

    feature - shows interesting info about the selected countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from "axios"
+import './App.css'
 
 function App() {
 
   const [countryName, setCountryName] = useState('')
+  const [countryInfo, setCountryInfo] = useState<undefined | any>(undefined)
 
-  const COUNTRIES_BASE_URL = "https://restcountries.com/v3.1"
+  const COUNTRIES_BASE_URL = "https://restcountries.com/v2"
 
   const search = () => {
     axios.get(COUNTRIES_BASE_URL + "/name/" + countryName).then(res => {
-      console.log(res.data)
+      setCountryInfo(res.data)
+    }).catch(err => {
+      console.log("country not found")
+      setCountryInfo(undefined)
     })
   }
 
@@ -29,7 +34,29 @@ function App() {
 
       <p>You have entered {countryName}</p>
 
-      <div id="country-result">This will show the result</div>
+      {countryInfo === undefined ? (
+        <p>Country not found</p>
+      ) : (
+        <div id="country-result">
+          <h1 id="country-result">
+            {countryInfo[0].name} ({countryInfo[0].nativeName})
+          </h1>
+          <img src={countryInfo[0].flag} alt="no flag found" />
+          <p>
+            Population: {countryInfo[0].population.toLocaleString('en', { maximumFractionDigits: 0 })}
+          </p>
+          <p>
+            Languages: {countryInfo[0].languages[0].name}
+          </p>
+          <p>
+            Currencies: {countryInfo[0].currencies[0].name} ({countryInfo[0].currencies[0].code})
+          </p>
+          <p>
+            Continent: {countryInfo[0].subregion}
+          </p>
+        </div>
+      )}
+
     </div>
   );
 }
